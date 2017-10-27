46f8cf8aaa331f2857063469fd0de7af239591a2
Ron Kurr <kurr@kurron.org>
2017-10-27 09:40:19 -0400

    first commit

---
+++ src/main/groovy/org/kurron/dual/DualApplication.groovy
@@ -0,0 +1,12 @@
+package org.kurron.dual
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class DualApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run DualApplication, args
+	}
+}
